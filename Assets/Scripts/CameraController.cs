71404910db752e3b4f28f60950462fb471385405
markmnl <markmnl@outlook.com>
2015-01-23 23:39:38 +0800

    Actually added stuff

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	const float CameraHeight = 10.0f;
+
+    bool lastIsMouseButtonDown;
+    Vector2 lastMouseButtonDownPos;
+
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+        
+        var isMouseButtonDown = Input.GetMouseButtonDown(0);
+
+		if (isMouseButtonDown) {
+			if (lastIsMouseButtonDown) {
+				OnDragged(lastMouseButtonDownPos, Input.mousePosition);
+			}
+			lastMouseButtonDownPos = Input.mousePosition;
+        }
+		lastIsMouseButtonDown = isMouseButtonDown;
+	}
+
+    void OnDragged(Vector2 from, Vector2 to) {
+
+		if (from == to)
+			return;
+
+        var delta = (to - from) * Settings.Instance.CameraPanSensitvity;
+        var pos = camera.transform.position;
+
+		camera.transform.position = new Vector3(pos.x + delta.x, CameraHeight, pos.z + delta.y);
+    }
+
+}
