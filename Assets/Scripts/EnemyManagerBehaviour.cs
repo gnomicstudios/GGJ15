038947750d3283531117aa024ffe2fde27fa3e94
markmnl <markmnl@outlook.com>
2015-01-24 10:54:16 +0800

    Added MarkTest.unity

---
+++ Assets/Scripts/EnemyManagerBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyPoolBehaviour : MonoBehaviour {
+
+    public float enemySpawnRadius;
+    public int numberOfEnemiesInRadius;
+    public EnemyController enemyTemplate;
+
+    List<EnemyController> enemies;
+    PlayerController[] players;
+    
+	// Use this for initialization
+	void Start () 
+    {
+        enemies = new List<EnemyController>();
+        players = GameObject.FindObjectsOfType<PlayerController>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for each player check there are so many enemies near by
+        foreach(var player in players)
+        {
+            var num = 0;
+            foreach (var enemy in enemies)
+            {
+                var dist = Vector3.Distance(player.transform.position, enemy.transform.position);
+                if (dist < enemySpawnRadius)
+                {
+ 
+                }
+            }
+        }
+	}
+
+    EnemyController CreateEnemy(Transform centre, float radius)   
+    {
+        var enemy = (EnemyController)GameObject.Instantiate(enemyTemplate);
+
+        return enemy;
+    }
+}
---
+++ Assets/Scripts/EnemyPoolBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyManagerBehaviour : MonoBehaviour {
+
+    public int initalNumberOfEnemies;
+    public EnemyController enemyTemplate;
+
+    List<EnemyController> enemies;
+    
+
+	// Use this for initialization
+	void Start () 
+    {
+        enemies = new List<EnemyController>();
+        for (var i = 0; i < initalNumberOfEnemies; i++)
+        {
+            var enemy = CreateEnemy();
+            enemies.Add(enemy);
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+	}
+
+    EnemyController CreateEnemy()   
+    {
+        return (EnemyController)GameObject.Instantiate(enemyTemplate);
+    }
+}
