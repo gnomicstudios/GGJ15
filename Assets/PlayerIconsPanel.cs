c035d728b56b5608225fe4c8846e9e0ead0fee18
Aranda Morrison <aranda_morrison@yahoo.com.au>
2015-01-25 15:27:46 +0800

    Resources die, multiple players (disabled)

---
+++ Assets/PlayerIconsPanel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerIconsPanel : MonoBehaviour
+{
+    float width;
+    RectTransform rectXform;
+    Players players;
+
+	// Use this for initialization
+	void Start () {
+        players = FindObjectOfType<Players> ();
+        rectXform = this.GetComponent<RectTransform> ();
+        width = rectXform.rect.width;
+        Debug.Log (width);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rectXform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, players.activePlayers.Count * width);
+
+        int i = 0;
+        var icons = this.GetComponentsInChildren<PlayerIcon> ();
+        foreach (var playerIcon in icons)
+        {
+            if (i < players.activePlayers.Count)
+            {
+                playerIcon.player = players.activePlayers[i];
+                playerIcon.gameObject.SetActive(true);
+                (playerIcon.transform as RectTransform).anchoredPosition = new Vector3(5 + width * i, 0.0f);
+            }
+            else
+            {
+                playerIcon.gameObject.SetActive(false);
+            }
+            i++;
+        }
+	}
+}
---
+++ Assets/ResourceController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceController : MonoBehaviour
+{
+    internal bool hasSpawnedPlayer = false;
+    private Players playerSpawner;
+    Health health;
+    Healer healer;
+    Flame flame;
+    SafeZone safeZone;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerSpawner = FindObjectOfType<Players> ();
+        healer = GetComponentInChildren<Healer> ();
+        healer.OnStartedHealingPlayer += OnStartedHealingPlayer;
+
+        health = GetComponentInChildren<Health> ();
+        flame = GetComponentInChildren<Flame> ();
+        safeZone = GetComponentInChildren<SafeZone> ();
+	}
+
+    void OnStartedHealingPlayer (PlayerController obj)
+    {
+        if (!hasSpawnedPlayer)
+        {
+            var dirn = RandomExtensions.GetOnUnitCircleXZ();
+            var newPlayer = playerSpawner.InstantiateNewPlayer(this.transform.position + dirn * 3.0f);
+            newPlayer.SetTarget(this.transform.position + dirn * 10.0f);
+            newPlayer.SetIsAIControlled(true);
+            hasSpawnedPlayer = true;
+            Debug.Log("Spawned new player!");
+        }
+    }
+
+    void Update()
+    {
+        if (health.currentHealth <= 0.0f)
+        {
+            flame.gameObject.SetActive(false);
+            healer.enabled = false;
+            safeZone.renderer.enabled = false;
+            this.enabled = false;
+        }
+
+    }
+}
