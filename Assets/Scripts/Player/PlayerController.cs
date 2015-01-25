04eda24d984b45a07fd4faa0162abaf38a706139
BobSmun <somemadeupname@gmail.com>
2015-01-25 08:47:53 +0800

    Add speech bubble

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -50,17 +50,22 @@ public class PlayerController : MonoBehaviour, ISelectableEntity
         float h = transform.rigidbody.velocity.x;
         anim.SetFloat("HorizontalVelocity", System.Math.Abs(h));
         // If the input is moving the player doesn't match direction facing
-        if(Mathf.Abs(h) > 0.01
-            && (h < 0) == facingRight)
+        if((Mathf.Abs(h) > 0.05 && (h < 0) == facingRight)
+            || (Mathf.Abs(h) < 0.05 && !facingRight))
         {
-            // Switch the way the player is labelled as facing.
-            facingRight = !facingRight;
-            
-            // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
-            theScale.x *= -1;
-            transform.localScale = theScale;
+            Flip();
         }
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+        
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
     void OnMouseDown()
